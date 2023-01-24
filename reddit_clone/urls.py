cb5c52e5dba7f4fc765716a833c853b65fa44454
Matthew Murnaghan <matt.murnaghan@outlook.ie>
2023-01-24 19:27:17 +0000

    Add temporary css file and update post detail view

--- reddit_clone/urls.py
+++ reddit_clone/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('news_app.urls'), name='news_app_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
